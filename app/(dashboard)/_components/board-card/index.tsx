5b8f288425baefe6904aba11b34d57fd071fa40c
Maestro1982 <kris.tote@telenet.be>
2024-02-01 15:02:47 +0100

    modify board-list.tsx and new-board-button.tsx in app (dashboard) _components folder, index.tsx in app (dashboard) _components board-card folder, board.ts in convex folder, create actions.tsx, confirm-modal.tsx in components folder, use-rename-modal.ts in store folder, install alert-dialog, dropdown-menu and skeleton from shadcn,

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -4,6 +4,11 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { formatDistanceToNow } from 'date-fns';
 import { useAuth } from '@clerk/nextjs';
+import { MoreHorizontal } from 'lucide-react';
+
+import { Skeleton } from '@/components/ui/skeleton';
+
+import { Actions } from '@/components/actions';
 
 import { Overlay } from './overlay';
 import { Footer } from './footer';
@@ -40,6 +45,11 @@ export const BoardCard = ({
         <div className='relative flex-1 bg-amber-50'>
           <Image src={imageUrl} alt={title} fill className='object-fit' />
           <Overlay />
+          <Actions id={id} title={title} side='right'>
+            <button className='absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity px-3 py-2 outline-none'>
+              <MoreHorizontal className='text-white opacity-75 hover:opacity-100 transition-opacity' />
+            </button>
+          </Actions>
         </div>
         <Footer
           isFavorite={isFavorite}
@@ -51,5 +61,13 @@ export const BoardCard = ({
         />
       </div>
     </Link>
+  );
+};
+
+BoardCard.Skeleton = function BoardCardSkeleton() {
+  return (
+    <div className='aspect-[100/127] rounded-lg overflow-hidden'>
+      <Skeleton className='w-full h-full' />
+    </div>
   );
 };
--- app/(dashboard)/_components/new-board-button.tsx
+++ app/(dashboard)/_components/new-board-button.tsx
@@ -1,8 +1,13 @@
 'use client';
 
 import { Plus } from 'lucide-react';
+import { toast } from 'sonner';
 
 import { cn } from '@/lib/utils';
+
+import { useApiMutation } from '@/hooks/use-api-mutation';
+
+import { api } from '@/convex/_generated/api';
 
 interface NewBoardButtonProps {
   orgId: string;
@@ -10,13 +15,28 @@ interface NewBoardButtonProps {
 }
 
 export const NewBoardButton = ({ orgId, disabled }: NewBoardButtonProps) => {
+  const { mutate, pending } = useApiMutation(api.board.create);
+
+  const onClick = () => {
+    mutate({
+      orgId,
+      title: 'Untitled',
+    })
+      .then((id) => {
+        toast.success('Board created');
+        // TODO: redirect to /board/${id}
+      })
+      .catch(() => toast.error('Failed to create board'));
+  };
+
   return (
     <button
-      disabled={disabled}
-      onClick={() => {}}
+      disabled={pending || disabled}
+      onClick={onClick}
       className={cn(
         'col-span-1 aspect-[100/127] bg-blue-500 rounded-lg hover:bg-blue-800 flex flex-col items-center justify-center py-6',
-        disabled && 'opacity-75'
+        (pending || disabled) &&
+          'opacity-75 hover:bg-blue-600 cursor-not-allowed'
       )}
     >
       <div />
